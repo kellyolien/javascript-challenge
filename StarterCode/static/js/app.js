51cd487749500dd53e52cec27a0cf58c5fef13ec
Kelly OlienC:/Users/Kelly/anaconda3/python.exe <kdubois77@hotmail.com>
2021-01-23 22:19:35 -0600

    added table

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(data)
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+ 
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
